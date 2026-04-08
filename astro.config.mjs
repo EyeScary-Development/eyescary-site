41a825c88a34426d167aa68cd707aad2234deb11
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2026-04-08 16:12:07 +0100

    remove some PII and change domains for archiving

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://eyescary.uk'
+    site: 'https://eyescary.c48.uk'
 });
